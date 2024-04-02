bf113bf1755086a1ce4dd27b34a77acb5cc0c0ec
henry tanoh <henry.tanoh@heex.io>
2024-04-02 07:53:28 +0200

    [master] write bf to disk

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"github.com/spaolacci/murmur3"
 	"math"
+	"flag"
+	"strings"
+	"bufio"
+	"os"
 )
 
 // Bloom filter structure
@@ -21,17 +25,71 @@ func main() {
 	flag.StringVar(&buildFlag, "build", "", "file.txt")
 	flag.Parse()
 
-	if buildFlag != "" {
-		build(buildFlag)
+	bf := NewBloomFilter(102401, 0.01)
+	if  buildFlag != "" {
+		err := FillDataInFilter(bf, buildFlag)
+		if err != nil {
+			fmt.Println("Error filling bloom filter:", err)
+		}
+		return
 	}
+
+
 }
 
-func FillDataInFilter(filter *BloomFilter, data string) {
+func FillDataInFilter(filter *BloomFilter, filepath string) error {
 	// read each line of the file and add it to the filter
-	scanner,  _ := bufio.NewScanner(strings.NewReader(data))
-	for _, item := range data {
-		filter.Add(item)
+	file, err := os.Open(filepath)
+	if err != nil {
+		return err
 	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		words := strings.Fields(scanner.Text())
+		for _, word := range words {
+			filter.Add(word)
+		}
+	}
+	// Check for scanner errors
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return err
+	}
+	return nil
+}
+
+// saveBloomFilter saves the Bloom filter to disk with a header
+func saveBloomFilter(bf *BloomFilter, filePath string) error {
+	// Create or truncate the file
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Write header to file
+	header := make([]byte, 12) // 4 bytes for identifier, 2 bytes for version number, 2 bytes for number of hash functions, 4 bytes for number of bits
+	copy(header[:4], "CCBF")    // Identifier
+	binary.BigEndian.PutUint16(header[4:6], 1) // Version number (example: 1)
+	binary.BigEndian.PutUint16(header[6:8], uint16(bf.NumHashFunctions))
+	binary.BigEndian.PutUint32(header[8:12], uint32(bf.NumBits))
+	if _, err := file.Write(header); err != nil {
+		return err
+	}
+
+	// Write bit array to file
+	for _, bit := range bf.BitArray {
+		var b byte
+		if bit {
+			b = 1
+		}
+		if _, err := file.Write([]byte{b}); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // calculateNumBits calculates the number of bits needed in the Bloom filter
