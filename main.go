4e88c99721dc9a41515063faa71fcc09e3ebdeda
dev-station-wsl-ubuntu-22 <thenry702@gmail.com>
2024-04-02 22:12:15 +0200

    Save to disk in binary format and load

--- main.go
+++ main.go
@@ -1,39 +1,57 @@
 package main
 
 import (
-	"fmt"
-	"github.com/spaolacci/murmur3"
-	"math"
-	"flag"
-	"strings"
 	"bufio"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"math"
 	"os"
+	"strings"
+
+	"github.com/spaolacci/murmur3"
 )
 
 // Bloom filter structure
 type BloomFilter struct {
-	numBits int
-	numHashFunctions int
-	bitArray []bool
+	numBits          uint32
+	numHashFunctions uint16
+	bitArray         []bool
 }
 
-func main() {
+var (
+	buildTime string
+	version   string
+)
 
-	fmt.Println("Lovely")
+const bloomFilterFilePath = "words.bf"
+
+func main() {
 
 	buildFlag := ""
 	flag.StringVar(&buildFlag, "build", "", "file.txt")
 	flag.Parse()
 
-	bf := NewBloomFilter(102401, 0.01)
-	if  buildFlag != "" {
+	bf := NewBloomFilter(100000, 0.00001)
+	if buildFlag != "" {
 		err := FillDataInFilter(bf, buildFlag)
 		if err != nil {
 			fmt.Println("Error filling bloom filter:", err)
 		}
 		return
+	} else {
+		bf, err := LoadBloomFilter(bloomFilterFilePath)
+		if err != nil {
+			fmt.Println("Error in loading Bloom Filter in memory")
+			return
+		}
+		for _, word := range flag.Args() {
+			// Perform spell check using Bloom filter
+			if !bf.Contains(word) {
+				fmt.Println(word + " is misspelled")
+			}
+		}
 	}
-
 
 }
 
@@ -56,6 +74,8 @@ func FillDataInFilter(filter *BloomFilter, filepath string) error {
 		fmt.Println("Error reading file:", err)
 		return err
 	}
+
+	saveBloomFilter(filter, bloomFilterFilePath)
 	return nil
 }
 
@@ -69,41 +89,101 @@ func saveBloomFilter(bf *BloomFilter, filePath string) error {
 	defer file.Close()
 
 	// Write header to file
-	header := make([]byte, 12) // 4 bytes for identifier, 2 bytes for version number, 2 bytes for number of hash functions, 4 bytes for number of bits
-	copy(header[:4], "CCBF")    // Identifier
+	header := make([]byte, 12)                 // 4 bytes for identifier, 2 bytes for version number, 2 bytes for number of hash functions, 4 bytes for number of bits
+	copy(header[:4], "CCBF")                   // Identifier
 	binary.BigEndian.PutUint16(header[4:6], 1) // Version number (example: 1)
-	binary.BigEndian.PutUint16(header[6:8], uint16(bf.NumHashFunctions))
-	binary.BigEndian.PutUint32(header[8:12], uint32(bf.NumBits))
+	binary.BigEndian.PutUint16(header[6:8], uint16(bf.numHashFunctions))
+	binary.BigEndian.PutUint32(header[8:12], bf.numBits)
 	if _, err := file.Write(header); err != nil {
 		return err
 	}
 
+	numBytes := (bf.numBits + 7) / 8 // Round up to the nearest whole number of bytes
+
+	// Convert the bit array to a byte slice
+	bitArrayBytes := make([]byte, numBytes)
+	var i uint32
+	for i = 0; i < bf.numBits; i++ {
+		if bf.bitArray[i] {
+			bitArrayBytes[i/8] |= 1 << (i % 8)
+		}
+	}
+
 	// Write bit array to file
-	for _, bit := range bf.BitArray {
-		var b byte
-		if bit {
-			b = 1
-		}
-		if _, err := file.Write([]byte{b}); err != nil {
-			return err
-		}
+	if _, err := file.Write(bitArrayBytes); err != nil {
+		return err
 	}
 
 	return nil
 }
 
+// Load bloom filter in memory
+func LoadBloomFilter(filePath string) (*BloomFilter, error) {
+	// Open the file
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Read the header
+	header := make([]byte, 12)
+	if _, err := file.Read(header); err != nil {
+		return nil, err
+	}
+
+	// Check the header
+	if string(header[:4]) != "CCBF" {
+		return nil, fmt.Errorf("invalid header")
+	}
+	version := binary.BigEndian.Uint16(header[4:6])
+	numHashFunctions := binary.BigEndian.Uint16(header[6:8])
+	numBits := binary.BigEndian.Uint32(header[8:12])
+	if version != 1 {
+		return nil, fmt.Errorf("unsupported version")
+	}
+
+	if numHashFunctions == 0 {
+		return nil, fmt.Errorf("invalid numHashFunctions")
+	}
+
+	if numBits == 0 {
+		return nil, fmt.Errorf("invalid numBits")
+	}
+
+	// Read the bit array
+	bitArrayBytes := make([]byte, (numBits+7)/8) // Round up to the nearest whole number of bytes
+	if _, err := file.Read(bitArrayBytes); err != nil {
+		return nil, err
+	}
+
+	// Convert the bit array bytes to a boolean array
+	bitArray := make([]bool, numBits)
+	var i uint32
+	for i = 0; i < numBits; i++ {
+		bitArray[i] = (bitArrayBytes[i/8] & (1 << (i % 8))) != 0
+	}
+
+	bf := &BloomFilter{
+		numBits:          numBits,
+		numHashFunctions: numHashFunctions,
+		bitArray:         bitArray,
+	}
+	return bf, nil
+}
+
 // calculateNumBits calculates the number of bits needed in the Bloom filter
-func calculateNumBits(numItems int, falsePositiveProb float64) int {
-    return int(-(float64(numItems) * math.Log(falsePositiveProb)) / (math.Log(2) * math.Log(2)))
+func calculateNumBits(numItems uint32, falsePositiveProb float64) uint32 {
+	return uint32(-(float64(numItems) * math.Log(falsePositiveProb)) / (math.Log(2) * math.Log(2)))
 }
 
 // calculateNumHashFunctions calculates the number of hash functions needed in the Bloom filter
-func calculateNumHashFunctions(numBits, numItems int) int {
-    return int(float64(numBits) / float64(numItems) * math.Log(2))
+func calculateNumHashFunctions(numBits, numItems uint32) uint16 {
+	return uint16(float64(numBits) / float64(numItems) * math.Log(2))
 }
 
 // BloomFilter constructor
-func NewBloomFilter(numItems int, falsePositiveErrorRate float64) *BloomFilter{
+func NewBloomFilter(numItems uint32, falsePositiveErrorRate float64) *BloomFilter {
 	bf := &BloomFilter{}
 	bf.numBits = calculateNumBits(numItems, falsePositiveErrorRate)
 	bf.numHashFunctions = calculateNumHashFunctions(bf.numBits, numItems)
@@ -113,16 +193,18 @@ func NewBloomFilter(numItems int, falsePositiveErrorRate float64) *BloomFilter{
 }
 
 func (bf *BloomFilter) Add(item string) {
-	for i := 0; i < bf.numHashFunctions; i++ {
+	var i uint16
+	for i = 0; i < bf.numHashFunctions; i++ {
 		hashIndex := murmurHash(item, i) % bf.numBits
 		bf.bitArray[hashIndex] = true
 	}
 }
 
 func (bf *BloomFilter) Contains(item string) bool {
-	for i := 0; i < bf.numHashFunctions; i++ {
+	var i uint16
+	for i = 0; i < bf.numHashFunctions; i++ {
 		hashIndex := murmurHash(item, i) % bf.numBits
-		if bf.bitArray[hashIndex] == false {
+		if !bf.bitArray[hashIndex] {
 			return false
 		}
 	}
@@ -130,8 +212,8 @@ func (bf *BloomFilter) Contains(item string) bool {
 }
 
 // murmurHash generates a hash value for the given item and seed using MurmurHash3
-func murmurHash(item string, seed int) int {
-    hash := murmur3.New32WithSeed(uint32(seed))
-    hash.Write([]byte(item))
-    return int(hash.Sum32())
+func murmurHash(item string, seed uint16) uint32 {
+	hash := murmur3.New32WithSeed(uint32(seed))
+	hash.Write([]byte(item))
+	return hash.Sum32()
 }
